c4c7f9a1b0cda7e3521aa99605cf0f04cc4b3575
SparksFlash <ug2102056@cse.pstu.ac.bd>
2025-03-19 20:30:24 +0600

    manage-admin' '

--- admin/index.php
+++ admin/index.php
@@ -1,26 +1,68 @@
 <html>
-    <head>
-        <title>Food Order Website - Home Page </title>
-    </head>
 
-    <body>
-        <!-- Main section start -->
-         <div class="menu">
-            Menu Goes Here
-         </div>
-        <!-- Main section start -->
+<head>
+   <title>Food Order Website - Home Page </title>
+   <link rel="stylesheet" href="../css/admin.css">
+</head>
 
-        <!-- Main Content Section -->
-         <div class="">
-            <Main> Content Goes Here</Main>
-         </div>
-        <!-- Main Content Section ends -->
+<body>
+   <!-- Main section start -->
+   <div class="menu text-center">
+      <div class="wrapper">
 
-        <!-- Footer Section start -->
-         <div class="footer">
-            Footer Goes Here
+         <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">Admin</a></li>
+            <li><a href="#">Category</a></li>
+            <li><a href="#">Food</a></li>
+            <li><a href="#">Order</a></li>
+         </ul>
+      </div>
+   </div>
+   <!-- Main section start -->
+
+   <!-- Main Content Section -->
+   <div class="main-content">
+      <div class="wrapper">
+         <h1>DASHBOARD</h1>
+
+         <div class="col-4">
+            <h1>5</h1>
+            <br />
+            Categories
          </div>
-         <!-- Footer Section ends-->
-        
-    </body>
+
+         <div class="col-4">
+            <h1>5</h1>
+            <br />
+            Categories
+         </div>
+
+         <div class="col-4">
+            <h1>5</h1>
+            <br />
+            Categories
+         </div>
+
+         <div class="col-4">
+            <h1>5</h1>
+            <br />
+            Categories
+         </div>
+
+         <div class="clearfix"></div>
+      </div>
+   </div>
+   <!-- Main Content Section ends -->
+
+   <!-- Footer Section start -->
+   <div class="footer">
+      <div class="wrapper">
+         <p class="text-center">&#169;2025 All rights reserved.</p>
+      </div>
+   </div>
+   <!-- Footer Section ends-->
+
+</body>
+
 </html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+    <title>Food Order Website - Home Page </title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Main section start -->
+    <div class="menu text-center">
+        <div class="wrapper">
+
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Main section start -->
+
+    <!-- Main Content Section -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+
+
+        </div>
+    </div>
+    <!-- Main Content Section ends -->
+
+    <!-- Footer Section start -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">&#169;2025 All rights reserved.</p>
+        </div>
+    </div>
+    <!-- Footer Section ends-->
+
+</body>
+
+</html>

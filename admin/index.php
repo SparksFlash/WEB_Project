7d8099cf98c6ad77c7ea8276b6cd4137cc6c4786
SparksFlash <ug2102056@cse.pstu.ac.bd>
2025-03-19 18:52:55 +0600

    structure

--- admin/index.php
+++ admin/index.php
@@ -5,5 +5,22 @@
 
     <body>
         <!-- Main section start -->
+         <div class="menu">
+            Menu Goes Here
+         </div>
+        <!-- Main section start -->
+
+        <!-- Main Content Section -->
+         <div class="">
+            <Main> Content Goes Here</Main>
+         </div>
+        <!-- Main Content Section ends -->
+
+        <!-- Footer Section start -->
+         <div class="footer">
+            Footer Goes Here
+         </div>
+         <!-- Footer Section ends-->
+        
     </body>
 </html>

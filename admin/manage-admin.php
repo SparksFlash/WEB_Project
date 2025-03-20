839897df922f5c04e7a3e3944d1a915287429d4a
SparksFlash <ug2102056@cse.pstu.ac.bd>
2025-03-20 17:05:55 +0600

    secondary-button

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,7 +5,52 @@
     <div class="wrapper">
         <h1>Manage Admin</h1>
 
+        <br /><br />
 
+        <!-- Button for add an Admin -->
+        <a href="#" class="button-primary">Add Admin</a>
+
+        <br /><br /><br />
+
+        <table class="table-full">
+            <tr>
+                <th>Sl. No.</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+
+            <tr>
+                <td>1.</td>
+                <td>Sourav</td>
+                <td>SparksFlash</td>
+                <td>
+                    <a href="#" class="button-secondary">Update Admin</a>
+                    <a href="#" class="button-secondary">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2.</td>
+                <td>Sourav</td>
+                <td>SparksFlash</td>
+                <td>
+                    Update Admin
+                    Delete Admin
+                </td>
+            </tr>
+
+            <tr>
+                <td>3.</td>
+                <td>Sourav</td>
+                <td>SparksFlash</td>
+                <td>
+                    Update Admin
+                    Delete Admin
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
 <!-- Main Content Section ends -->

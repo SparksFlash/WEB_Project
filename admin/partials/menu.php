2ffdee15ebdaec88622b6d4a52009eb7e3f93bd5
SparksFlash <ug2102056@cse.pstu.ac.bd>
2025-05-14 05:03:25 +0600

    login page okay

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -1,5 +1,8 @@
-<?php include('../config/constants.php'); ?>
-
+<?php
+session_start();
+include('../config/constants.php');
+include('login-check.php');
+?>
 <html>
 
 <head>
@@ -8,7 +11,6 @@
 </head>
 
 <body>
-    <!-- Main section start -->
     <div class="menu text-center">
         <div class="logo-background">
             <a href="#" title="Logo">
@@ -26,7 +28,7 @@
                 <li><a href="manage-category.php">Category</a></li>
                 <li><a href="manage-food.php">Food</a></li>
                 <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
             </ul>
         </div>
     </div>
-    <!-- Main section start -->
